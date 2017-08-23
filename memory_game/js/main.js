0d762b2a1fd010da89c8b50a4fba8ca6559fa1f9
bjudas03 <31077242+bjudas03@users.noreply.github.com>
2017-08-23 16:11:47 -0700

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,28 @@
  console.log("Up and running!");
- var cardOne = "queen";
- var cardTwo = "queen";
- var cardThree = "king";
- var cardFour = "king";
+ console.log("User flipped " + 'cardOne');
+ console.log("User flipped " + 'cardTwo');
 
- console.log("User flipped " + cardOne);
- console.log("User flipped " + cardThree)
+ var cards = ["queen", "queen", "king", "king"];
+ var cardsInPlay = [];
+
+ var cardOne = cards[0];
+ var cardTwo = cards[3];
+
+ cardsInPlay.push('cardOne');
+ cardsInPlay.push('cardTwo');
+
+ cardsInPlay.length;
+
+if (cardsInPlay.length === 2) {
+     else if (cardsInPlay[0] === cardsInPlay[1]);
+	  window.alert("You Found a match!");
+  }  else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	  window.alert("Sorry! Try again.");
+  }
+
+
+
+
+
+
+
