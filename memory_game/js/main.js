2850feb5202958d205947b3c66e9f0921ae202ae
bjudas03 <31077242+bjudas03@users.noreply.github.com>
2017-08-24 14:29:36 -0700

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,78 @@
  console.log("Up and running!");
- console.log("User flipped " + 'cardOne');
- console.log("User flipped " + 'cardTwo');
 
- var cards = ["queen", "queen", "king", "king"];
+ var cards = [
+   {
+ 	   	rank: "Queen",
+ 	 	suit: "Hearts",
+ 	 	cardImage: "memory_game/images/queen-of-hearts.png"
+ 	},
+ 	{
+ 		rank: "Queen",
+ 		suit: "Diamonds",
+ 		cardImage: "memory_game/images/queen-of-diamonds.png"
+ 	},
+ 	{
+ 		rank: "King",
+ 		suit: "Hearts",
+ 		cardImage: "memory_game/images/king-of-hearts.png"
+ 	},
+ 	{
+ 		rank: "King",
+ 		suit: "Diamonds",
+ 		cardImage: "memory_game/images/king-of-diamonds.png"
+ 	}
+ ];
  var cardsInPlay = [];
-
- var cardOne = cards[0];
- var cardTwo = cards[3];
-
- cardsInPlay.push('cardOne');
- cardsInPlay.push('cardTwo');
-
  cardsInPlay.length;
 
-if (cardsInPlay.length === 2) {
-     else if (cardsInPlay[0] === cardsInPlay[1]);
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  	window.alert("You found a match!");
+  } else {
+  	window.alert("Sorry, try again!");
+  }
+};
+/*
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+*/
+
+
+//flip card function below:
+
+var flipCard = function (cardId) {
+var cardOne = cards[0];
+var cardTwo = cards[2];
+  console.log("User flipped " + /*cards[cardId]*/cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
+//**Redundant code**// 	
+/* 	
+var cardTwo = cards[2];
+ console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[CardId]);
+*/
+
+//**Broken Code??**//
+/*
+   if (cardsInPlay.length === 2)
+  } else if (cardsInPlay[0] === cardsInPlay[1]);
 	  window.alert("You Found a match!");
-  }  else if (cardsInPlay[0] !== cardsInPlay[1]) {
+  } else if (cardsInPlay[0] !== cardsInPlay[1]) {
 	  window.alert("Sorry! Try again.");
   }
+  */
+  checkForMatch();
 
+};
+flipCard(0);
+flipCard(2);
 
 
 
